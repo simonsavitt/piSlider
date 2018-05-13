be5f0ddea9c7f1d9146c918823fa1bb0c277c98a
Simon Savitt <simon.savitt@gmail.com>
2018-05-13 10:23:22 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -12,11 +12,12 @@ function setup() {
 }
 
 function draw() {
-  background(255);
+  background('grey');
   textSize(21);
-  fill(0);
+  fill(255);
   var d = piSlider.value();
   text(Math.PI.toFixed(d), 10, 10, windowWidth, windowHeight);
+  fill(0);
   text("decimal places", piSlider.width + 30 + x, y + 15);
   text(d, x + 12 + (d*offset) , piSlider.height + y + 20);
   console.log(piSlider.width)
