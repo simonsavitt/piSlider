42b11d0ca36bb8f37a689292881aaf2859ddcdc8
Simon Savitt <simon.savitt@gmail.com>
2018-05-13 10:12:39 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+x = 100;
+y = 50;
+offset = 3.8;
+
+function setup() {
+  createCanvas(600, 100);
+
+  piSlider = createSlider(0, 48, 2);
+  piSlider.style('width', '200px')
+  piSlider.position(x, y);
+
+}
+
+function draw() {
+  background(255);
+  textSize(21);
+  fill(0);
+  var d = piSlider.value();
+  text(Math.PI.toFixed(d), 10, 10, windowWidth, windowHeight);
+  text("decimal places", piSlider.width + 30 + x, y + 15);
+  text(d, x + 12 + (d*offset) , piSlider.height + y + 20);
+  console.log(piSlider.width)
+}
